f76f289abb2bc0dd1fe69b818fddc2793e3eb920
Karim90403 <karim90403@gmail.com>
2022-08-08 20:59:33 +0300

    Add new functions

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,51 @@
 <template>
-<sity></sity>
+<img v-if="isInSettings" src="./components/icons/back.png" alt="Settings" class="navigation__icon" @click="toggleNav"/>
+<img v-else src="./components/icons/settings.png" alt="Settings" class="navigation__icon" @click="toggleNav"/>
+<div v-if="isInSettings"> 
+  <h1 class="navigation__title">Settings</h1>
+  <form @submit.prevent="addSity" class="settings__form">
+    <input v-model="newSity" class="settings__input" type="text" name="newSity" placeholder="New Sity" required>
+    <button class="settings__button">Add</button>    
+  </form>
+  <div v-for='sity in sityArray' :key="sity.id" class="list">
+    {{sity.name}}
+    <div @click="removeSity(sity)">X</div>
+  </div>
+</div>
+<sity v-else
+  v-for='sity in sityArray' :key="sity.id"
+  v-bind:sity="sity.name"
+></sity>
 </template>
 
 <script>
-import sity from '.components/sity.vue';
+import sity from './components/sity.vue';
+let id = 0
 export default{
   data() {
     return {
-      
+      isInSettings: false,
+      newSity: '',
+      sityArray: [
+        {id: id++, name: 'Moscow'},
+        {id: id++, name: 'Berlin'},
+        {id: id++, name: 'London'}
+      ]
+    }
+  },
+  components: {
+    sity
+  },
+  methods: {
+    addSity() {
+      this.sityArray.push({ id: id++, name: this.newSity })
+      this.newSity = ''
+    },
+    removeSity(sity) {
+      this.sityArray = this.sityArray.filter((t) => t !== sity)
+    },
+    toggleNav(){
+      this.isInSettings = !this.isInSettings
     }
   }
 }
--- src/components/sity.vue
+++ src/components/sity.vue
@@ -30,116 +30,112 @@
       </div>
     </div>
   </div>
-  <input type="text" v-model="sity">
-    <div @click="getWeather(sity)">Button</div>
 </template>
 
-  <script>
-    import axios from 'axios';
-    let url = 'https://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=caac442a9f6f6b2f02fa20c006534b5d';
-    const temperatureUnit = '˚C';
-    const visibilityUnit = 'km';
-    const humidityUnit = '%';
-    const pressureUnit = 'hPa';
-    const windUnit = 'm/s';
-    export default {
-      data() {
-        return {
-          sity: 'Moscow',
-          cityName: '--',
-          countryName: '--',
-          description: '--',
-          feelslike: '--',
-          icon: '50d',
-          temperature: '--˚',
-          details: {
-            visibility: 0,
-            pressure: 0,
-            humidity: 0,
-            wind: 0
-          }
-        };
-      },
-      methods: {
-          async getWeather(sity) {
-            try {
-              url = `https://api.openweathermap.org/data/2.5/weather?q=${sity}&appid=caac442a9f6f6b2f02fa20c006534b5d`;
-              console.log(url);
-              let res = await axios.get(url)
-              console.log(res.data);
-              this.cityName = res.data.name;
-              this.countryName = res.data.sys.country;
-              this.description = res.data.weather[0].description;
-              this.feelslike = getTemperature(res.data.main.feels_like)
-              this.temperature = getTemperature(res.data.main.temp)
-              this.icon = res.data.weather[0].icon;
-              this.details = getDetails(res.data);
-          } catch (err) { console.log(err); }
-        },
-      },
-      async mounted() {
-        try {
-          let res = await axios.get(url)
-          console.log(res.data);
-          this.cityName = res.data.name;
-          this.countryName = res.data.sys.country;
-          this.description = res.data.weather[0].description;
-          this.feelslike = getTemperature(res.data.main.feels_like)
-          this.temperature = getTemperature(res.data.main.temp)
-          this.icon = res.data.weather[0].icon;
-          this.details = getDetails(res.data);
-          renderDayOrNight(res.data);
+<script>
+  import axios from 'axios';
+  // let url = `https://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=caac442a9f6f6b2f02fa20c006534b5d`;
+  const temperatureUnit = '˚C';
+  const visibilityUnit = 'km';
+  const humidityUnit = '%';
+  const pressureUnit = 'hPa';
+  const windUnit = 'm/s';
+  export default {
+    props:['sity'],
+    data() {
+      return {
+        cityName: '--',
+        countryName: '--',
+        description: '--',
+        feelslike: '--',
+        icon: '50d',
+        temperature: '--˚',
+        details: {
+          visibility: 0,
+          pressure: 0,
+          humidity: 0,
+          wind: 0
+        }
+      };
+    },
+    methods: {
+        async getWeather(sity) {
+          try {
+            url = `https://api.openweathermap.org/data/2.5/weather?q=${sity}&appid=caac442a9f6f6b2f02fa20c006534b5d`;
+            let res = await axios.get(url)
+            this.cityName = res.data.name;
+            this.countryName = res.data.sys.country;
+            this.description = res.data.weather[0].description;
+            this.feelslike = getTemperature(res.data.main.feels_like)
+            this.temperature = getTemperature(res.data.main.temp)
+            this.icon = res.data.weather[0].icon;
+            this.details = getDetails(res.data);
         } catch (err) { console.log(err); }
-      }
-    };
+      },
+    },
+    async mounted() {
+      try {
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${this.sity}&appid=caac442a9f6f6b2f02fa20c006534b5d`;
+        let res = await axios.get(url)
+        this.cityName = res.data.name;
+        this.countryName = res.data.sys.country;
+        this.description = res.data.weather[0].description;
+        this.feelslike = getTemperature(res.data.main.feels_like)
+        this.temperature = getTemperature(res.data.main.temp)
+        this.icon = res.data.weather[0].icon;
+        this.details = getDetails(res.data);
+        renderDayOrNight(res.data);
+      } catch (err) { console.log(err); }
+    }
+  };
 
 function getDetails(data) {
-  let item = data;
-  let pressure = getValueWithUnit(item.main.pressure, pressureUnit);
-  let humidity = getValueWithUnit(item.main.humidity, humidityUnit);
-  let visibility = getVisibility(item.visibility);
-  let wind = getValueWithUnit(item.wind.speed, windUnit);
+let item = data;
+let pressure = getValueWithUnit(item.main.pressure, pressureUnit);
+let humidity = getValueWithUnit(item.main.humidity, humidityUnit);
+let visibility = getVisibility(item.visibility);
+let wind = getValueWithUnit(item.wind.speed, windUnit);
 
-  return {
-    pressure: pressure,
-    humidity: humidity,
-    visibility: visibility,
-    wind: wind
-  }
+return {
+  pressure: pressure,
+  humidity: humidity,
+  visibility: visibility,
+  wind: wind
+}
 }
 
 function getValueWithUnit(value, unit) {
-  return `${value}${unit}`;
+return `${value}${unit}`;
 }
 
 function getTemperature(value) {
-  var roundedValue = Math.round(value-273.15);
-  return getValueWithUnit(roundedValue, temperatureUnit);
+var roundedValue = Math.round(value-273.15);
+return getValueWithUnit(roundedValue, temperatureUnit);
 }
 
 function getVisibility(value) {
-  var roundedValue = Math.round(value/1000);
-  return getValueWithUnit(roundedValue, visibilityUnit);
+var roundedValue = Math.round(value/1000);
+return getValueWithUnit(roundedValue, visibilityUnit);
 }
 
 function isDay(data) {
-  let sunrise = data.sys.sunrise * 1000;
-  let sunset = data.sys.sunset * 1000;
+let sunrise = data.sys.sunrise * 1000;
+let sunset = data.sys.sunset * 1000;
 
-  let now = Date.now();
-  return (now > sunrise && now < sunset);
+let now = Date.now();
+return (now > sunrise && now < sunset);
 }
 
 function renderDayOrNight(data) {
-  let attrName = isDay(data) ? 'day':'night';
-  transition();
-  document.documentElement.setAttribute('data-theme', attrName);
+let attrName = isDay(data) ? 'day':'night';
+transition();
+document.documentElement.setAttribute('data-theme', attrName);
 }
 function transition() {
-  document.documentElement.classList.add('transition');
-  setTimeout(function() {
-    document.documentElement.classList.remove('transition');
-  }, 4000)
+document.documentElement.classList.add('transition');
+setTimeout(function() {
+  document.documentElement.classList.remove('transition');
+}, 4000)
 }
 
-  </script>
+</script>
